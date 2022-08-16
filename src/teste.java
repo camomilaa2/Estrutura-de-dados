24c9e6380501d14820eb096e7c4355c2b538ed27
Camila Soares da Silva <12109716@aluno.newtonpaiva.br>
2022-08-16 20:11:42 -0300

    primeiro commit

---
+++ src/teste.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String args[]){
+        int[] vetor = new int[10];
+        int aux;
+
+        Scanner entrada = new Scanner(System.in);
+    }
+}
