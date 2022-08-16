52927ff01fd4bcb8d7748e05b4331629c16524ff
Camila Soares da Silva <12109716@aluno.newtonpaiva.br>
2022-08-16 20:52:27 -0300

    3

--- src/teste.java
+++ src/teste.java
@@ -1,10 +1,33 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class teste {
     public static void main(String args[]){
         int[] vetor = new int[10];
-        int aux;
+        Random numerosRandomicos = new Random();
+        int maxValue = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
 
-        Scanner entrada = new Scanner(System.in);
+        for(int i = 0; i < vetor.length; i++){
+            vetor[i] = numerosRandomicos.nextInt(999);
+        }
+
+        for(int i = 0; i < vetor.length; i++){
+            if(minValue > vetor[i]){
+                minValue = vetor[i];
+            }
+            if(maxValue < vetor[i]){
+                maxValue = vetor[i];
+            }
+        }
+
+        System.out.printf("Maior valor: %d", maxValue);
+        System.out.printf("\nMenor valor: %d\n", minValue);
+
+        for(int i = 0; i< vetor.length; i++){
+            System.out.println(vetor[i]);
+        }
+
+
     }
 }
